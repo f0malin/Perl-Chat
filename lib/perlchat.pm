c6dfac31eb8a3d43e1a280b73b2d29aced2dda07
Achilles Xu <formalin14@gmail.com>
2013-06-28 11:56:41 +0800

    login ok

--- lib/perlchat.pm
+++ lib/perlchat.pm
@@ -1,10 +1,43 @@
 package perlchat;
+
 use Dancer ':syntax';
+use PerlChat::Plugin::DB;
+use Digest::SHA1 qw(sha1_hex);
 
 our $VERSION = '0.1';
 
 get '/' => sub {
-    template 'index';
+    if (!session('nick')) {
+        redirect "/login";
+        return;
+    }
+    "ok";
 };
+
+get '/login' => sub {
+    template 'login';
+};
+
+post '/login' => sub {
+    my $nick = param("nick");
+    my $pass = param("pass");
+    my $user = db->get_collection("users")->find_one({nick => $nick});
+    if ($user) {
+        if (password($pass) eq $user->{'pass'}) {
+            session 'nick' => $nick;
+            redirect "/";
+        } else {
+            template "login" => { errmsg => "密码错误", nick => $nick };
+        }
+    } else {
+        db->get_collection("users")->insert({nick => $nick, pass => password($pass)});
+        session 'nick' => $nick;
+        redirect "/";
+    }
+};
+
+sub password {
+    return sha1_hex('sklsfhi@#%#@$FHLK' . $_[0]);
+}
 
 true;
